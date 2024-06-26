b141527b81910964904d959db854bb33eeb73a77
nekosu <liao.hy@outlook.com>
2024-06-26 21:34:54 +0800

    feat: maa log

--- pkgs/main/src/main.ts
+++ pkgs/main/src/main.ts
@@ -1,13 +1,15 @@
 // it is import to use `import * as`
 import * as maa from '@nekosu/maa-node'
 import { BrowserWindow, app } from 'electron'
-import * as path from 'path'
 
 import pkg from '../../../package.json'
 import { setupIpc } from './ipc'
 import { createWindow } from './window'
 
+process.chdir(app.getPath('userData'))
+
 console.log(maa.version(), maa.AdbController.agent_path())
+maa.set_global_option('LogDir', '.')
 
 app.setAboutPanelOptions({
   applicationName: pkg.name,
