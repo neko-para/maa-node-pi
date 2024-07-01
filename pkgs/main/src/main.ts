7a1a13c925777510b530b6f278fbbab9f8610503
nekosu <liao.hy@outlook.com>
2024-07-01 15:35:42 +0800

    feat: workaround for mac app

--- pkgs/main/src/main.ts
+++ pkgs/main/src/main.ts
@@ -1,6 +1,7 @@
 // it is import to use `import * as`
 import * as maa from '@nekosu/maa-node'
-import { BrowserWindow, app } from 'electron'
+import { app } from 'electron'
+import * as path from 'path'
 
 import pkg from '../../../package.json'
 import { setupIpc } from './ipc'
@@ -10,6 +11,10 @@ process.chdir(app.getPath('userData'))
 
 console.log(maa.version(), maa.AdbController.agent_path(), process.cwd())
 maa.set_global_option('LogDir', '.')
+
+if (process.platform === 'darwin') {
+  process.env.PATH = process.env.PATH + path.delimiter + '/usr/local/bin'
+}
 
 app.setAboutPanelOptions({
   applicationName: pkg.name,
